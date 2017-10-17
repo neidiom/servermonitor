7167ed4309c935bbbc7042633da75da183dfef99
neidiom <h.nedim@gmail.com>
2017-10-17 16:17:07 +0200

    update tests

--- spec/mailq_spec.rb
+++ spec/mailq_spec.rb
@@ -2,12 +2,25 @@ require 'spec_helper'
 require 'servermonitor/mailq'
 
 describe ServerMonitor::MailqConfiguration do
-  it "Creates new instance of the Configuration class with default parameter values" do
+  it "Creates a new instance of the MailqConfiguration class with default parameter values" do
     config = ServerMonitor::MailqConfiguration.new
     expect(config.path).to be       == "/opt/zimbra/common/sbin/postqueue -p"
     expect(config.grep).to be       == "/bin/grep"
     expect(config.critical).to be   == 50
     expect(config.warning).to be    == 40
     expect(config.exit_codes).to be == true
+  end
+end
+
+describe ServerMonitor::Mailq do
+  it "Configures the Mailq class method" do
+    ServerMonitor::Mailq.configure do |config|
+      config.path       = "/usr/bin/mailq"
+      config.grep       = "/usr/bin/grep"
+      config.exit_codes = true
+    end
+    expect(ServerMonitor::Mailq.config.path).to be        == "/usr/bin/mailq"
+    expect(ServerMonitor::Mailq.config.grep).to be        == "/usr/bin/grep"
+    expect(ServerMonitor::Mailq.config.exit_codes).to be  == true
   end
 end
--- spec/megacli_vd_status_spec.rb
+++ spec/megacli_vd_status_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'servermonitor/megacli_vd_status'
 
 describe ServerMonitor::VDConfiguration do
-  it "Creates new instance of Configuration class with default parameter values" do
+  it "Creates a new instance of the VDConfiguration class with default parameter values" do
     config = ServerMonitor::VDConfiguration.new
     expect(config.grep).to be             == "/bin/grep"
     expect(config.exit_codes).to be       == false
@@ -13,5 +13,29 @@ describe ServerMonitor::VDConfiguration do
     expect(config.smtp_port).to be        == nil
     expect(config.smtp_username).to be    == nil
     expect(config.smtp_password).to be    == nil
+  end
+end
+
+describe ServerMonitor::MegaCliVDStatus do
+  it "Configures the MegaCliVDStatus class method" do
+    ServerMonitor::MegaCliVDStatus.configure do |config|
+      config.megacli        = "/usr/sbin/megacli"
+      config.grep           = "/usr/bin/grep"
+      config.exit_codes     = false
+      config.email_to       = "example@example.to"
+      config.email_from     = "example@example.from"
+      config.smtp_address   = "server"
+      config.smtp_port      = "25"
+      config.smtp_username  = "username"
+      config.smtp_password  = "password"
+    end
+    expect(ServerMonitor::MegaCliVDStatus.config.megacli).to be       == "/usr/sbin/megacli"
+    expect(ServerMonitor::MegaCliVDStatus.config.grep).to be          == "/usr/bin/grep"
+    expect(ServerMonitor::MegaCliVDStatus.config.email_to).to be      == "example@example.to"
+    expect(ServerMonitor::MegaCliVDStatus.config.email_from).to be    == "example@example.from"
+    expect(ServerMonitor::MegaCliVDStatus.config.smtp_address).to be  == "server"
+    expect(ServerMonitor::MegaCliVDStatus.config.smtp_port).to be     == "25"
+    expect(ServerMonitor::MegaCliVDStatus.config.smtp_username).to be == "username"
+    expect(ServerMonitor::MegaCliVDStatus.config.smtp_password).to be == "password"
   end
 end
