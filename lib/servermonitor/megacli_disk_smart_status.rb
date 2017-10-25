1654fc73327b558121ad8107e55ea64c52e071cc
neidiom <h.nedim@gmail.com>
2017-10-25 14:00:37 +0200

    fix a wrong megacli parameter when showing each disk status

--- lib/servermonitor/megacli_disk_smart_status.rb
+++ lib/servermonitor/megacli_disk_smart_status.rb
@@ -47,19 +47,20 @@ module ServerMonitor
       in_in.each do |i|
         # Regex matching
         Regexp.new('Drive\s*has\s*flagged\s*a\s*S\.M\.A\.R\.T\s*alert\s*:\s*Yes').match?(i) ? smart_status = "ALERT: YES".upcase : smart_status = "ALERT: NO".upcase
-        # Display S.M.A.R.T Status
+        # Display S.M.A.R.T Status and do a clean exit if parameter exit_codes is enabled
         if smart_status == "ALERT: NO"
           puts "S.M.A.R.T drive status is " + smart_status.to_s + " on hostname " + fhostname
           puts exit 0 unless self.config.exit_codes == false
         else
+          # Display S.M.A.R.T Status and exit with error if exit_codes parameter is enabled
           puts "S.M.A.R.T drive status is " + smart_status.to_s + " on hostname " + fhostname
           puts exit 1 unless self.config.exit_codes == false
         end
         # Send email if email_to address is not empty AND if a disk does not return an "Alert: NO"
         if self.config.email_to != nil && smart_status != "ALERT: NO"
           time    = Time.now.strftime("%d.%m.%Y %H:%M")
-          subject = "Daily RAID check STARTED on #{fhostname} at #{time}. RAID STATE: #{smart_status}."
-          body    = "Daily RAID check: #{smart_status}"
+          subject = "RAID check STARTED on #{fhostname} at #{time}. RAID STATE: #{smart_status}."
+          body    = "RAID check: #{smart_status}"
           email   = ServerMonitor::EMail.new(self.config.email_from, self.config.email_to, self.config.smtp_address, self.config.smtp_port, self.config.smtp_username, self.config.smtp_password, subject, body)
           email.deliver
         end
